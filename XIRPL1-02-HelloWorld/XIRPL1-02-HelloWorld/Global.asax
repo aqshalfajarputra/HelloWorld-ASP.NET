a268c5f1627cb95611387ae97d7ce4febb7de71d
Aqshal Fajar Putra <aqshalfajarputra0226@gmail.com>
2017-01-06 19:32:51 +0700

    initial commit

---
+++ XIRPL1-02-HelloWorld/XIRPL1-02-HelloWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XIRPL1_02_HelloWorld.MvcApplication" Language="C#" %>
---
+++ XIRPL1-02-HelloWorld/XIRPL1-02-HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XIRPL1_02_HelloWorld.Startup))]
+namespace XIRPL1_02_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
